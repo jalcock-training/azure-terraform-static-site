86fe9924a43400ee216f6a2d6c69f25337cdc1bb
James Alcock <alcock.james.training@proton.me>
2026-01-09 10:47:55 +1030

    feature: linting and formatting

--- infra/live/dev/main.tf
+++ infra/live/dev/main.tf
@@ -2,7 +2,7 @@ module "static_site" {
   source = "../../modules/static_site"
 
   # required inputs
-  site_name                = "static-site-ja-dev01"
+  site_name           = "static-site-ja-dev01"
   resource_group_name = "rg-staticsite-dev"
   location            = "australiaeast"
 
